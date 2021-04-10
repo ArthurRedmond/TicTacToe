528e5fe4ff294c722b406b05177c994c0ac2ea9d
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2021-04-10 17:50:50 -0400

    Added comments to logic

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -43,8 +44,10 @@ public class Main {
         char[] winHorizontalX = {'X', 'X', 'X'};
         char[] winHorizontalY = {'O', 'O', 'O'};
 
+        // Checks if the number of turns is out of count. Should be no more than 5:4
         if (Math.abs(xCount - oCount) > 1) {
             return "Impossible";
+            // Checks to see if the game is possible if there are two "winners" horizontal
         } else if ((Arrays.equals(gameplay[0], winHorizontalX) ||
                 Arrays.equals(gameplay[1], winHorizontalX) ||
                 Arrays.equals(gameplay[2], winHorizontalX)) &&
@@ -52,6 +55,7 @@ public class Main {
                         Arrays.equals(gameplay[1], winHorizontalY) ||
                         Arrays.equals(gameplay[2], winHorizontalY))) {
             return "Impossible";
+            // Checks to see if the game is possible if there are two "winners" vertical
         } else if ((gameplay[0][0] == 'X' && gameplay[1][0] == 'X' && gameplay[2][0] == 'X') ||
                 (gameplay[0][1] == 'X' && gameplay[1][1] == 'X' && gameplay[2][1] == 'X') ||
                 (gameplay[0][2] == 'X' && gameplay[1][2] == 'X' && gameplay[2][2] == 'X') &&
@@ -59,25 +63,31 @@ public class Main {
                 (gameplay[0][1] == 'O' && gameplay[1][1] == 'O' && gameplay[2][1] == 'O') ||
                 (gameplay[0][2] == 'O' && gameplay[1][2] == 'O' && gameplay[2][2] == 'O')) {
             return "Impossible";
+            // If X is in all three spaces horizontal
         } else if (Arrays.equals(gameplay[0], winHorizontalX) ||
                 Arrays.equals(gameplay[1], winHorizontalX) ||
                 Arrays.equals(gameplay[2], winHorizontalX)) {
             return "X wins";
+            // If O is in all three spaces horizontal
         } else if (Arrays.equals(gameplay[0], winHorizontalY) ||
                 Arrays.equals(gameplay[1], winHorizontalY) ||
                 Arrays.equals(gameplay[2], winHorizontalY)) {
             return "O wins";
+            // if X is in all three spaces vertical
         } else if ((gameplay[0][0] == 'X' && gameplay[1][0] == 'X' && gameplay[2][0] == 'X') ||
                 (gameplay[0][1] == 'X' && gameplay[1][1] == 'X' && gameplay[2][1] == 'X') ||
                 (gameplay[0][2] == 'X' && gameplay[1][2] == 'X' && gameplay[2][2] == 'X')) {
             return "X wins";
+            // if O is in all three spaces vertical
         } else if ((gameplay[0][0] == 'O' && gameplay[1][0] == 'O' && gameplay[2][0] == 'O') ||
                 (gameplay[0][1] == 'O' && gameplay[1][1] == 'O' && gameplay[2][1] == 'O') ||
                 (gameplay[0][2] == 'O' && gameplay[1][2] == 'O' && gameplay[2][2] == 'O')) {
             return "O wins";
+            // If above has not caught, are there still moves?
         } else if (containsSpaces(gameplay)) {
             return "Game not finished";
         }
+        // Return "Draw" if no winner determined
         return output;
     }
 
