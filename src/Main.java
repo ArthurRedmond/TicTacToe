0a153e3c3e13769c062d732ef036076c91c08496
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2021-04-10 22:40:06 -0400

    Gameplay works

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,31 @@ public class Main {
         char[][] gameplay = startGame();
 
         printGame(gameplay);
-        playerX(gameplay);
-        printGame(gameplay);
-        //        System.out.println(result(inputArray));
+        playGame(gameplay);
+
 
     }
 
     public static char[][] startGame() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter cells: ");
-        String input = scanner.nextLine().toUpperCase();
-        char[][] inputArray = new char[3][3];
-        inputArray[0] = input.substring(0, 3).toCharArray();
-        inputArray[1] = input.substring(3, 6).toCharArray();
-        inputArray[2] = input.substring(6, 9).toCharArray();
+        char[][] inputArray = {{'_', '_', '_'}, {'_', '_', '_'}, {'_', '_', '_'}};
         return inputArray;
+    }
+
+    public static void playGame(char[][] gameplay) {
+        while (result(gameplay).length() < 2) {
+            playerX(gameplay);
+            printGame(gameplay);
+            if (result(gameplay).length() > 2) {
+                break;
+            }
+            playerO(gameplay);
+            printGame(gameplay);
+            if (result(gameplay).length() > 2) {
+                break;
+            }
+        }
+        System.out.println(result(gameplay));
     }
 
     public static void playerX(char[][] gameplay) {
@@ -37,7 +47,7 @@ public class Main {
             scanner.nextLine();
 
             // Validate entries are base 10 digits
-            row = Character.digit(rowString.charAt(0),10);
+            row = Character.digit(rowString.charAt(0), 10);
             column = Character.digit(columnString.charAt(0), 10);
 
             if (row == -1 || column == -1) {
@@ -53,6 +63,36 @@ public class Main {
             break;
         }
         gameplay[row - 1][column - 1] = 'X';
+    }
+
+    public static void playerO(char[][] gameplay) {
+        Scanner scanner = new Scanner(System.in);
+        int row = -1;
+        int column = -1;
+
+        while (true) {
+            System.out.print("Enter the coordinates: ");
+            String rowString = scanner.next();
+            String columnString = scanner.next();
+            scanner.nextLine();
+
+            // Validate entries are base 10 digits
+            row = Character.digit(rowString.charAt(0), 10);
+            column = Character.digit(columnString.charAt(0), 10);
+
+            if (row == -1 || column == -1) {
+                System.out.println("You should enter numbers!");
+                continue;
+            } else if (row < 1 || row > 3 || column < 1 || column > 3) {
+                System.out.println("Coordinates should be from 1 to 3!");
+                continue;
+            } else if (gameplay[row - 1][column - 1] != ' ' && gameplay[row - 1][column - 1] != '_') {
+                System.out.println("This cell is occupied! Choose another one!");
+                continue;
+            }
+            break;
+        }
+        gameplay[row - 1][column - 1] = 'O';
     }
 
     public static void printGame(char[][] gameplay) {
@@ -71,17 +111,16 @@ public class Main {
     }
 
     public static String result(char[][] gameplay) {
-        String output = "Draw";
-
         int xCount = xCount(gameplay);
         int oCount = oCount(gameplay);
+        String returnString = "";
 
         char[] winHorizontalX = {'X', 'X', 'X'};
         char[] winHorizontalY = {'O', 'O', 'O'};
 
         // Checks if the number of turns is out of count. Should be no more than 5:4
         if (Math.abs(xCount - oCount) > 1) {
-            return "Impossible";
+            returnString = "Impossible";
             // Checks to see if the game is possible if there are two "winners" horizontal
         } else if ((Arrays.equals(gameplay[0], winHorizontalX) ||
                 Arrays.equals(gameplay[1], winHorizontalX) ||
@@ -89,49 +128,48 @@ public class Main {
                 (Arrays.equals(gameplay[0], winHorizontalY) ||
                         Arrays.equals(gameplay[1], winHorizontalY) ||
                         Arrays.equals(gameplay[2], winHorizontalY))) {
-            return "Impossible";
+            returnString = "Impossible";
             // Checks to see if the game is possible if there are two "winners" vertical
         } else if (((gameplay[0][0] == 'X' && gameplay[1][0] == 'X' && gameplay[2][0] == 'X') ||
                 (gameplay[0][1] == 'X' && gameplay[1][1] == 'X' && gameplay[2][1] == 'X') ||
                 (gameplay[0][2] == 'X' && gameplay[1][2] == 'X' && gameplay[2][2] == 'X')) &&
                 ((gameplay[0][0] == 'O' && gameplay[1][0] == 'O' && gameplay[2][0] == 'O') ||
-                (gameplay[0][1] == 'O' && gameplay[1][1] == 'O' && gameplay[2][1] == 'O') ||
-                (gameplay[0][2] == 'O' && gameplay[1][2] == 'O' && gameplay[2][2] == 'O'))) {
-            return "Impossible";
+                        (gameplay[0][1] == 'O' && gameplay[1][1] == 'O' && gameplay[2][1] == 'O') ||
+                        (gameplay[0][2] == 'O' && gameplay[1][2] == 'O' && gameplay[2][2] == 'O'))) {
+            returnString = "Impossible";
             // If X is in all three spaces horizontal
         } else if (Arrays.equals(gameplay[0], winHorizontalX) ||
                 Arrays.equals(gameplay[1], winHorizontalX) ||
                 Arrays.equals(gameplay[2], winHorizontalX)) {
-            return "X wins";
+            returnString = "X wins";
             // If O is in all three spaces horizontal
         } else if (Arrays.equals(gameplay[0], winHorizontalY) ||
                 Arrays.equals(gameplay[1], winHorizontalY) ||
                 Arrays.equals(gameplay[2], winHorizontalY)) {
-            return "O wins";
+            returnString = "O wins";
             // if X is in all three spaces vertical
         } else if ((gameplay[0][0] == 'X' && gameplay[1][0] == 'X' && gameplay[2][0] == 'X') ||
                 (gameplay[0][1] == 'X' && gameplay[1][1] == 'X' && gameplay[2][1] == 'X') ||
                 (gameplay[0][2] == 'X' && gameplay[1][2] == 'X' && gameplay[2][2] == 'X')) {
-            return "X wins";
+            returnString = "X wins";
             // if O is in all three spaces vertical
         } else if ((gameplay[0][0] == 'O' && gameplay[1][0] == 'O' && gameplay[2][0] == 'O') ||
                 (gameplay[0][1] == 'O' && gameplay[1][1] == 'O' && gameplay[2][1] == 'O') ||
                 (gameplay[0][2] == 'O' && gameplay[1][2] == 'O' && gameplay[2][2] == 'O')) {
-            return "O wins";
+            returnString = "O wins";
             // If X is in diagonal positions
         } else if ((gameplay[0][0] == 'X' && gameplay[1][1] == 'X' && gameplay[2][2] == 'X') ||
                 (gameplay[0][2] == 'X' && gameplay[1][1] == 'X' && gameplay[2][0] == 'X')) {
-            return  "X wins";
+            returnString = "X wins";
             // If O is in diagonal positions
         } else if ((gameplay[0][0] == 'O' && gameplay[1][1] == 'O' && gameplay[2][2] == 'O') ||
                 (gameplay[0][2] == 'O' && gameplay[1][1] == 'O' && gameplay[2][0] == 'O')) {
-            return  "X wins";
-            // If above has not caught, are there still moves?
-        } else if (containsSpaces(gameplay)) {
-            return "Game not finished";
+            returnString = "O wins";
+        } else if (!containsSpaces(gameplay)){
+            // Return "Draw" if no winner determined
+            returnString = "Draw";
         }
-        // Return "Draw" if no winner determined
-        return output;
+        return returnString;
     }
 
     public static int xCount(char[][] gameplay) {
